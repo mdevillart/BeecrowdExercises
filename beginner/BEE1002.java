9452b42738b961645d0e5924553dd60b57e78cbc
mdevillart <mdevillart@outlook.com>
2022-01-30 17:54:01 -0300

    exercise 1002 added

--- beginner/BEE1002.java
+++ beginner/BEE1002.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class BEE1002 {
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class BEE1002 {
+    public static void main(String[] args) throws IOException {
+        double  n = 3.14159;
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+        double r = scanner.nextDouble();
+        double area = n*(r*r);
+        System.out.println("A=" + String.format("%.4f", area));
+    }
 }
