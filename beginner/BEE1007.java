1f7e2c02ad1ce98a305277327cc307d78c69644f
mdevillart <mdevillart@outlook.com>
2022-01-31 20:36:15 -0300

    added exercise 1007

---
+++ beginner/BEE1007.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BEE1007 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+        int D = scanner.nextInt();
+        int DIFERENCA = ((A * B) - (C * D));
+        System.out.println("DIFERENCA = " + DIFERENCA);
+    }
+}
