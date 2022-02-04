6339a77c4d9180a85ecdc440ccb7c482295aea8d
mdevillart <mdevillart@outlook.com>
2022-02-03 22:55:55 -0300

    exercise 1001 added

---
+++ beginner/BEE1011.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BEE1011 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        float radius = scanner.nextFloat();
+        double pi =  3.14159;
+        double volume = ((4/3.0) * pi * (Math.pow(radius, 3)));
+        System.out.println("VOLUME = " + String.format("%.3f", volume));
+    }
+}
